db1491d93136ca94574ab1360e294b8d3d86e97c
João Vitor Retamero <joaovretamero@gmail.com>
2022-09-26 15:17:45 -0300

    Word Count second iteration

--- word-count/lib/word_count.dart
+++ word-count/lib/word_count.dart
@@ -1,57 +1,22 @@
 class WordCount {
+  // After reading some solutions, I found that I could use a simple regular expression
+  // to match letters and digits (\w), the apostrophe, and ignore the rest
   Map<String, int> countWords(String phrase) {
-    var words = _splitWords(phrase);
-    Map<String, int> map = {};
+    var normalizedPrhase = phrase.toLowerCase();
+    var words = Map<String, int>();
+    Iterable<RegExpMatch> matches =
+        new RegExp(r"\w+('\w+)*").allMatches(normalizedPrhase);
 
-    for (var word in words) {
-      if (map.containsKey(word)) {
-        int count = map[word]!;
-        map[word] = count + 1;
+    for (var match in matches) {
+      var word = normalizedPrhase.substring(match.start, match.end);
+      if (words.containsKey(word)) {
+        var count = words[word]!;
+        words[word] = count + 1;
       } else {
-        map[word] = 1;
+        words[word] = 1;
       }
     }
 
-    return map;
-  }
-
-  Iterable<String> _splitWords(String phrase) sync* {
-    var buffer = StringBuffer();
-    List<int> codeUnits = phrase.codeUnits;
-    int? lastChar;
-
-    for (int i = 0; i < codeUnits.length; i++) {
-      int char = codeUnits[i];
-
-      if (_isDigit(char) || _isLetter(char)) {
-        if (_isApostrophe(char) &&
-            (!_isLetter(lastChar ?? 0) || i == codeUnits.length - 1)) {
-          continue;
-        }
-
-        buffer.write(String.fromCharCode(char));
-      } else if (buffer.isNotEmpty) {
-        yield buffer.toString().toLowerCase();
-        buffer.clear();
-      }
-
-      lastChar = char;
-    }
-
-    if (buffer.isNotEmpty) yield buffer.toString().toLowerCase();
-  }
-
-  bool _isDigit(int char) {
-    return char >= 48 && char <= 57;
-  }
-
-  bool _isLetter(int char) {
-    return (char >= 65 && char <= 90) ||
-        (char >= 97 && char <= 122) ||
-        _isApostrophe(char);
-  }
-
-  bool _isApostrophe(int char) {
-    return char == 39;
+    return words;
   }
 }
