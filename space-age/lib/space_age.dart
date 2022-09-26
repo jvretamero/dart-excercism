09c342773d68ee7d3193d6dba0c1ed889a0d47bf
João Vitor Retamero <joaovretamero@gmail.com>
2022-09-26 15:17:46 -0300

    Space Age (finished)

--- space-age/lib/space_age.dart
+++ space-age/lib/space_age.dart
@@ -1,3 +1,24 @@
 class SpaceAge {
-  // Put your code here
+  static const YEAR_IN_SECONDS = 31557600;
+
+  Map<String, double> planets = {
+    'Earth': 1,
+    'Mercury': 0.2408467,
+    'Venus': 0.61519726,
+    'Mars': 1.8808158,
+    'Jupiter': 11.862615,
+    'Saturn': 29.447498,
+    'Uranus': 84.016846,
+    'Neptune': 164.79132,
+  };
+
+  double age({
+    required String planet,
+    required int seconds,
+  }) {
+    double earthYears = seconds / YEAR_IN_SECONDS;
+    double years = earthYears / planets[planet]!;
+
+    return double.parse(years.toStringAsFixed(2));
+  }
 }
--- space-age/test/space_age_test.dart
+++ space-age/test/space_age_test.dart
@@ -13,30 +13,31 @@ void ageOnPlanetTests() {
   }, skip: false);
 
   test('age on Mercury', () {
-    expect(spaceAge.age(planet: 'Mercury', seconds: 2134835688), equals(280.88));
-  }, skip: true);
+    expect(
+        spaceAge.age(planet: 'Mercury', seconds: 2134835688), equals(280.88));
+  }, skip: false);
 
   test('age on Venus', () {
     expect(spaceAge.age(planet: 'Venus', seconds: 189839836), equals(9.78));
-  }, skip: true);
+  }, skip: false);
 
   test('age on Mars', () {
     expect(spaceAge.age(planet: 'Mars', seconds: 2129871239), equals(35.88));
-  }, skip: true);
+  }, skip: false);
 
   test('age on Jupiter', () {
     expect(spaceAge.age(planet: 'Jupiter', seconds: 901876382), equals(2.41));
-  }, skip: true);
+  }, skip: false);
 
   test('age on Saturn', () {
     expect(spaceAge.age(planet: 'Saturn', seconds: 2000000000), equals(2.15));
-  }, skip: true);
+  }, skip: false);
 
   test('age on Uranus', () {
     expect(spaceAge.age(planet: 'Uranus', seconds: 1210123456), equals(0.46));
-  }, skip: true);
+  }, skip: false);
 
   test('age on Neptune', () {
     expect(spaceAge.age(planet: 'Neptune', seconds: 1821023456), equals(0.35));
-  }, skip: true);
+  }, skip: false);
 }
